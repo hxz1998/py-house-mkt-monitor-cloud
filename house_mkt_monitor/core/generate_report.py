e1570f02346d313b41e00f49b88afe7af11fd044
cherry <1466947023@qq.com>
2025-02-27 22:07:33 +0800

    调整拉取数据的频率、调整报告输出格式和位置

--- house_mkt_monitor/core/generate_report.py
+++ house_mkt_monitor/core/generate_report.py
@@ -25,12 +25,21 @@ def fetch_data_from_db(db_path, table_name):
 def generate_markdown(column_names, rows, exclude_column=None):
     # 如果需要排除某一列，找到它的索引并过滤掉
     # 生成Markdown表格的标题行
-    markdown = "| " + " | ".join(column_names) + " |\n"
-    markdown += "| " + " | ".join(["---"] * len(column_names)) + " |\n"
+    index = [i for i, x in enumerate(column_names) if x == exclude_column]
+    if len(index) > 0:
+        logger.info(f"检测到需要排除的列：{index}: {[value for idx, value in enumerate(column_names) if idx in index]}")
+
+    # 生成表头
+    markdown = "| " + " | ".join(
+        [value for curr_index, value in enumerate(column_names) if curr_index not in index]) + " |\n"
+
+    # 生成分割符
+    markdown += "| " + " | ".join(["---"] * (len(column_names) - len(index))) + " |\n"
 
     # 生成Markdown表格的数据行
     for row in rows:
-        markdown += "| " + " | ".join(map(str, row)) + " |\n"
+        markdown += "| " + " | ".join(
+            map(str, [value for curr_index, value in enumerate(row) if curr_index not in index])) + " |\n"
 
     return markdown
 
@@ -45,10 +54,10 @@ def generate_report(db_path, table_name):
     column_names, rows = fetch_data_from_db(db_path, table_name)
 
     # 输出Markdown文件的路径
-    output_file = './report.md'
+    output_file = './README.md'
 
     # 生成Markdown文本
-    markdown = generate_markdown(column_names, rows)
+    markdown = generate_markdown(column_names, rows, exclude_column='raw_text')
 
     # 将Markdown文本保存到文件
     save_markdown_to_file(markdown, output_file)
